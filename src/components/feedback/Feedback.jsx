56cf601efcd6fce20cb2867a8444ff19b7abf46d
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-03-14 16:26:08 +0200

    3456

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -3,31 +3,27 @@ import React from 'react';
 import { AllButtons } from './feedback.styled';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-
-
   return (
     <div>
-
-      < AllButtons>
-    
-          {options.map(option => (
-            <button type="button" onClick={onLeaveFeedback} key={option} name ={option}>
-              {option}
-            </button>
-          ))}
-      
-      </ AllButtons>
-
+      <AllButtons>
+        {options.map(option => (
+          <button
+            type="button"
+            onClick={onLeaveFeedback}
+            key={option}
+            name={option}
+          >
+            {option}
+          </button>
+        ))}
+      </AllButtons>
     </div>
-
   );
 };
 
-
 FeedbackOptions.propTypes = {
   onLeaveFeedback: PropTypes.func.isRequired,
-  options: PropTypes.array.isRequired,
-}
-
+  options: PropTypes.string.isRequired,
+};
 
 export default FeedbackOptions;
